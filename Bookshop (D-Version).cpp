b065e4eda45aa2b1ee3b79079cefd71189db578e
Googl <crileyjm@gmail.com>
2012-11-19 21:25:13 -0500

    / ‘Bookshop (D-Version).cpp’

--- Bookshop (D-Version).cpp
+++ Bookshop (D-Version).cpp
@@ -7,45 +7,49 @@ using namespace std;
 // Prints stock shortage to screen
 void NotInStock(void)
  {
- 	cout << "Not in stock" << endl;
- 	
+  cout << "Not in stock" << endl;
+
  };  //End of function
- 
+
  //Basic Layout for table (Header).
 // setw(40) works better than \t
  void TableLayout()
- { 	
-  	cout << "|==========================================================================|" << '\n';
+ {
+    cout << "|==========================================================================|" << '\n';
     cout << '|' << setw(40) << "Bookshop Inventory" << setw(35) << '|' << '\n';  // use endl to push to screen
- 	cout << "|==========================================================================|" << endl;
- 	cout << "| Author's Name|";
-    cout << "| Book Title   |"; 
-    cout << "| Unit Price 	|";
-    cout << "| Publisher   	|"; 
-    cout << "| Stock Count 	|" << endl;
+  cout << "|==========================================================================|" << endl;
+  cout << "| Author's Name|";
+    cout << "| Book Title   |";
+    cout << "| Unit Price   |";
+    cout << "| Publisher    |";
+    cout << "| Stock Count  |" << endl;
  };
-    
+
 
 void search_data()
  {
- 	
-	char author_name[30];
-	char book_title[50];
-	
- 	cout <<"Enter Author's Name: " << endl;
- 	cin  >> author_name;
- 	cout <<"Enter Book Title: " << endl;
- 	cin  >> book_title;
- }   
- 
+
+//  char author_name[30];
+//  char book_title[50];
+  string author_name;
+  string book_title;
+
+  cout <<"Enter Author's Name: " << endl;
+//  cin  >> author_name;
+  getline(cin,author_name);
+  cout <<"Enter Book Title: " << endl;
+//  cin  >> book_title;
+  getline(cin,book_title);
+ }
+
 int main()
 {
-			
-	TableLayout();	
-	search_data();
 
-	//system("pause");
+  TableLayout();
+  search_data();
+
+  //system("pause");
 
 
-	return 0;
+  return 0;
 }
