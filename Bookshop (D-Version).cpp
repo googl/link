2d6607e49c9e2d7992749ea330a25971b5474fd2
Dwayne Reid <kooldknight@gmail.com>
2012-11-19 20:17:11 -0500

    / ‘Bookshop (D-Version).cpp’

--- Bookshop (D-Version).cpp
+++ Bookshop (D-Version).cpp
@@ -9,7 +9,20 @@ void NotInStock(void)
  	cout << "Not in stock" << endl;
  	
  };
-
+ 
+ void TableLayout()
+ {
+ 	public:
+ 	
+ 	cout << "| Author's Name 	|";
+    cout << "| Book Title       |"; 
+    cout << "| Unit Price 		|";
+    cout << "| Publisher   		|"; 
+    cout << "| Stock Count 		|";
+ };
+    
+    
+    
 int main()
 {
 	
