4e98a787229be62221c24e5cafc4983b07d0987e
Dwayne Reid <kooldknight@gmail.com>
2012-11-19 20:13:34 -0500

    + ‘Bookshop (D-Version).cpp’

---
+++ Bookshop (D-Version).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+// Prints stock shortage to screen
+void NotInStock(void)
+ {
+ 	cout << "Not in stock" << endl;
+ 	
+ };
+
+int main()
+{
+	
+	
+	//system("pause");
+	return 0;
+}
