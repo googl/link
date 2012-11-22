0c5eaa5dc5580a0139bfb7522cf52efa0a8f872c
Dwayne Reid <kooldknight@gmail.com>
2012-11-22 00:03:57 -0500

    / ‘Bookshop Inventory - Final.cpp’

--- Bookshop Inventory - Final.cpp
+++ Bookshop Inventory - Final.cpp
@@ -1,8 +1,8 @@
 //(c) Dwayne Reid && Carey Riley
 
-#include <iostream> /*Header Files*/
-#include <string> /*Header Files*/
-#include <iomanip>  /*Header Files*/
+#include <iostream>	/*Header Files*/
+#include <string>	/*Header Files*/
+#include <iomanip>	/*Header Files*/
 using namespace std;
 
 /*------------------------------------------------------------------*/
@@ -11,10 +11,10 @@ using namespace std;
 namespace {  // unnamed namespace make it true OOP
 void NotInStock(void)
  {
-  cerr << "Required copies not in stock" << endl;
+  cout << "Required copies not in stock" << endl;
 
  };  //End of function
-
+ 
 /*------------------------------------------------------------------*/
 
 //Basic Layout for table (Header).
@@ -24,8 +24,8 @@ void NotInStock(void)
  {
     cout << "|==========================================================================|" << '\n';
     cout << '|' << setw(40) << "Bookshop Inventory" << setw(35) << '|' << '\n';  // use endl to push to screen
-  cout << "|==========================================================================|" << endl;
-  cout << "| Author's Name|";
+	cout << "|==========================================================================|" << endl;
+	cout << "| Author's Name|";
     cout << "| Book Title   |";
     cout << "| Unit Price   |";
     cout << "| Publisher    |";
@@ -38,11 +38,16 @@ void NotInStock(void)
   {
     string author_name;
     string book_title;
+    int cop_ies;
 
     cout <<"Enter Author's Name: " << endl;
     getline(cin,author_name);
     cout <<"Enter Book Title: " << endl;
     getline(cin,book_title);
+    cout<<"Please enter the amount of copies needed"<<endl;
+    cin>>cop_ies;
+    
+    
 
 //Need to make a relational comparison between user's input and variables in database
 };
@@ -50,53 +55,107 @@ void NotInStock(void)
 
 /*------------------------------------------------------------------*/
 
-class test
+class book
 {
-  public:
-    string get_author(void)    { return author_;    }
-    string get_title(void)     { return title_;     }
-    double get_unitprice(void) { return unitprice_; }
-    string get_publisher(void) { return publisher_; }
-    int get_quantity(void)     { return quantity_;  }
+	private:
+	string author_name;
+	string book_title;
+	float Book_Price;
+	string Book_Publisher;
+	int Stock_Count;
+	public:
+	book(){};
+	~book(){};
+	string get_author(void)    {return author_name;}
+    string get_title(void)     {return book_title;}
+    double get_unitprice(void) {return Book_Price;}
+    string get_publisher(void) {return Book_Publisher;}
+    int get_avail_stock(void)  {return Stock_Count;}
+    void newrecord(void){}
+    
+	void SetAuthorName(string A_NTemp)
+    {
+    	author_name = A_NTemp;
+    }
+    void SetBookTitle(string B_tTemp)
+    {
+    	book_title = B_tTemp;
+    }
+    void SetBookPrice(string B_PTemp)
+    {
+    	Book_Price = B_PTemp;
+    }
+    void SetPublisher(string B_PubTemp)
+    {
+    	Book_Publisher = B_PubTemp;
+    }
+	void SetStock(int S_Temp)
+	{
+		Stock_Count = S_Temp;
+	}
+};
+void newrecord (void)
+{
+	string A_NTemp;
+	
+		
+	cout<<"Please enter the Author's Name."<<endl;
+	cin>>A_NTemp;
+	book::SetAuthorName(A_NTemp);
+	
+	
+}
 
-   test() : author_("Deitel & Deitel"),
-           title_("C++...Program"),
-           unitprice_(2500.00),
-           publisher_("Pearson"),
-           quantity_(15)
-           {}
-
-  private:
-    string author_;
-    string title_;
-    double unitprice_;  // double larger than float
-    string publisher_;
-    int quantity_;
-}; //End of test class
 
 /*------------------------------------------------------------------*/
-
-class book: public test
-{
-		
-};
 
 //Start of main function
 int main()
 {
+	int switchtrig;
+	
+	
+	
+	cout<<"---!Welcome to the Coursework #2 Bookshop Inventory Program!---"<<endl;
+	cout<<"-------------------------------------------------------------"<<endl;
+	cout<<"Please enter your number of choice for its corresponding task"<<endl;
+	cout<<"1. \t Enter a new book record."<<endl;
+	cout<<"2. \t Search for a book record."<<endl;
+	cout<<"3. \t Enter a new book record."<<endl;
+	 
+	cin>>switchtrig;
+	
+	switch(switchtrig){
+		case 1:
+			cout<<"You have selected to enter a new record"<<endl;
+			newrecord();
+			break;
+		case 2:
+			cout<<"You have selected to search for a book"<<endl;
+			search_data();
+			break;
+		default:
+			cout<<"Please enter a number from 1 to n"<<endl;
+			break;
+	}
+	
+	
+	
+	
+	
+	
 
   TableLayout(); //Runs Table Layout function (Header)
 
-  test test_obj; //Object for test function
-
+ 
+  
   search_data(); //Runs Search function (input from user)
-  cout << test_obj.get_author() << '\n';
+ // cout << test_obj.get_author() << '\n';
 
     //system("pause");
-  // to test variables
-  //cout << book_title << "by" << author_name;
+	// to test variables
+	//cout << book_title << "by" << author_name;
 
   return 0;
 }
 //End of program
-a
