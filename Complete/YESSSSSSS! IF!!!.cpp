c70df93f6e6969997e1ea505f36d624e63925873
Dwayne Reid <kooldknight@gmail.com>
2012-11-27 13:46:38 -0500

    / ‘Complete/YESSSSSSS! IF!!!.cpp’

--- Complete/YESSSSSSS! IF!!!.cpp
+++ Complete/YESSSSSSS! IF!!!.cpp
@@ -11,14 +11,10 @@ int main()
 	
 	cout <<"Please enter the author's name and title of book: " << endl;
 	cin  >>Temp_Name, Temp_Book;
-	
-	if(Temp_Name == AuthorName && Temp_Book == Auther_Book)
-	
-	cout <<"You've entered: " << Temp_Name << endl;
-	
-	else 
-	cout <<"Copies not in stock!" << endl;
-	
+
+	cout <<"Please enter the  title of book: " << endl;
+	cin  >> Temp_Book;
+
 	
 	return 0;
 }
