fe41903add59944bc5d71f247b7047bcaa2b9a63
Dwayne Reid <kooldknight@gmail.com>
2012-11-27 13:43:02 -0500

    / ‘Complete/YESSSSSSS! IF!!!.cpp’ / ‘Complete/YESSSSSSS! IF!!!.exe’ / ‘Complete/YESSSSSSS! IF!!!.o’

--- Complete/YESSSSSSS! IF!!!.cpp
+++ Complete/YESSSSSSS! IF!!!.cpp
@@ -5,18 +5,16 @@ using namespace std;
 
 int main()
 {
-	string Temp_Name;
-	string AuthorName = "Deitel & Deitel";
-	string Temp_Book;
-	string A_Book = "C++...Program";
+	string Temp_Name, AuthorName = "Deitel & Deitel";
+	string Temp_Book, Auther_Book = "C++...Program";
 	
 	
 	cout <<"Please enter the author's name and title of book: " << endl;
 	cin  >>Temp_Name, Temp_Book;
 	
-	if(Temp_Name == AuthorName && Temp_Book == A_Book)
+	if(Temp_Name == AuthorName && Temp_Book == Auther_Book)
 	
-	cout <<"You've entered: " << Temp_Name /*<< Temp_Book*/ << endl;
+	cout <<"You've entered: " << Temp_Name << endl;
 	
 	else 
 	cout <<"Copies not in stock!" << endl;
