5a1cede114ff8b51601d336b010682a8b866b1aa
Dwayne Reid <kooldknight@gmail.com>
2012-11-27 12:10:32 -0500

    + ‘Complete/C++ Coursework - 1.cpp’ + ‘Complete/C++ Coursework - 2 (Error).cpp’ + ‘Complete/C++ Coursework - 3 (Incomplete).cpp’

---
+++ Complete/C++ Coursework - 1.cpp
@@ -0,0 +1,79 @@
+/* © Copyright 2012 Dwayne Akeem Reid. All rights reserved */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*2a.*/
+class Date {
+    int day;
+    int month;
+    int year;
+  public:
+    Date(int, int, int) :
+      day(1),
+      month(1),
+      year(1970) {}
+  int getDay() {return day;}
+  int getMonth() {return month;}
+  int getYear() {return year;}
+};
+
+/*2b. InventoryItem containing Name, Cost, Quantity, DatePurchased(Object of the date Class),
+InventoryTotal(Static Variable)*/
+
+class InventoryItem {
+  std::string Name;
+  double Cost;
+  int Quantity;
+  Date DatePurchased;              /*Object of the date class*/
+  static int InventoryTotal;
+
+  public:
+  InventoryItem() :
+    Name(" "),
+    Cost(0.0),
+    Quantity(0),
+    DatePurchased(1, 1, 1970) {}
+
+    void setInventoryTotal(int amt) { InventoryTotal = amt; }
+    std::string getName() {return Name;}
+    double getCost() {return Cost;}
+    int getQuantity() {return Quantity;}
+    // Date getDatePurchased() { }
+    int getInventoryTotal() {return InventoryItem::InventoryTotal;}
+
+    /*2.c friend function to change quantity of Inventory item*/
+
+     friend void changeQuantity(int quan);  // { Quantity = quan; }
+     void changeQuantity(int quan) { Quantity = quan; }
+
+  // ~InventoryItem() {cout << "Object deleted.\n";}  // destructor
+     ~InventoryItem() {}
+  };
+
+  int main()  {
+    /*3. main function that instantiates 10 InventoryItem objects using arrays; should use 
+    friend function to change   the quantity of 4 InventoryItem objects*/
+
+  // InventoryItem testit[10];
+  InventoryItem *p_testit = new InventoryItem[10];
+   /* changing quantity works
+    testit.changeQuantity(10.00);
+   cout << testit.getQuantity();
+    */
+
+    for (int sub(0), quan(1); sub <= 3; ++sub, ++quan) {
+      p_testit[sub].changeQuantity(quan++);
+     // cout << testit[sub].getQuantity() << " in stock." << "\n";
+    }
+    int i(0);
+    while (i <= 3) {
+      cout << p_testit[i].getQuantity() << " in stock." << std::endl;
+      ++i;
+    }
+    delete [] p_testit;
+    
+	system ("pause");
+}
