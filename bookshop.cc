b88781a20bd125231de8c276ae4ed20c1eb52092
Googl <crileyjm@gmail.com>
2012-11-28 08:47:43 -0500

    / ‘bookshop.cc’

--- bookshop.cc
+++ bookshop.cc
@@ -60,6 +60,42 @@ namespace {
   using std::setw;
   using std::cout;
   using std::cerr;
+  using std::basic_string;
+
+// facilitates case-insensitive search
+struct ci_char_traits : public __gnu_cxx::char_traits<char>
+                // just inherit all the other functions
+                //  that we don't need to override
+  {
+    static bool eq( char c1, char c2 )
+      { return toupper(c1) == toupper(c2); }
+
+    static bool ne( char c1, char c2 )
+      { return toupper(c1) != toupper(c2); }
+
+    static bool lt( char c1, char c2 )
+      { return toupper(c1) <  toupper(c2); }
+
+    static int compare( const char* s1,
+                        const char* s2,
+                        size_t n ) {
+      return memicmp( s1, s2, n );
+             // if available on your compiler,
+             //  otherwise you can roll your own
+    }
+
+    static const char*
+    find( const char* s, int n, char a ) {
+      while( n-- > 0 && toupper(*s) != toupper(a) ) {
+          ++s;
+      }
+      return s;
+    }
+  };
+
+typedef basic_string<char, ci_char_traits> ci_string;
+
+
 
 // print stock shortage to screen
   void NotInStock(void) {cerr << "Required copies not in stock\n";}
@@ -99,19 +135,21 @@ namespace {
 // BookSearch(std::string, std::string) { }
 class Books {
   public:
-    Books() : author_("Deitel & Deitel"),
-              title_("C++...Program"),
-              unitprice_(2500.00),
-              publisher_("Pearson"),
-              quantity_(15)
-              {}
+    Books() { 
+      author_    = new[3] = {"Deitel & Deitel", };
+      title_     = new[3] = {"C++...Program", };
+      unitprice_ = new[3] = {2500.00};
+      publisher_ = new[3] = {"Pearson"};
+      quantity_  = new[3] = {15,15,15};
+    }
+    ~Books[]();  
 //      {cout << "|" <<  author_ << " | " << title_ << " | " << unitprice_ <<
 //      " | " << publisher_ << " | " << quantity_ <<  "|" << '\n';
 //      };
 //    double CustomerWants(int quantity) {quantity_ <=  quantity ?: NotInStock() : return quantity * unitprice_;
   private:
-    std::string author_;
-    std::string title_;
+    ::ci_string author_;
+    ::ci_string title_;
     double unitprice_;
     std::string publisher_;
     int quantity_;
@@ -119,7 +157,7 @@ class Books {
 
 
 int main() {
-NotInStock();  // here to test if function works
+//NotInStock();  // here to test if function works
 PrintStockTableHeader();
 // Books shelf;  // operator new should be used. how?
 // shelf;
